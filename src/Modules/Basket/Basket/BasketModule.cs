1e5adb203dac64e0ecbb7a1ef19613ce5e707577
shamsisadr01 <shamsisadr01@gmail.com>
2026-02-14 16:16:10 +0330

    Develop Basket Data-Infrastructure with EF Core Code First and PostgreSQL

--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
+using Shared.Data.Interceptor;
+using Shared.Data.Seed;
 
 namespace Basket
 {
@@ -9,20 +14,34 @@ namespace Basket
         public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration configuration)
         {
             // Add Service To Conatiner
-            //services
-            //    .AddApplicationServices()
-            //    .AddInfrastructureServices(configuration)
-            //    .AddApiServices(configuration)
+
+            // Data - Infrastructure Services
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<BasketDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseNpgsql(connectionString);
+            });
+
+          //  services.AddScoped<IDataSeeder, CatalogDataSeeder>();
+
             return services;
         }
 
         public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
         {
-            // Add Service To Conatiner
-            //services
-            //    .UseApplicationServices()
-            //    .UseInfrastructureServices(configuration)
-            //    .UseApiServices(configuration)
+            // Configure the request pipline
+
+            // 1. Use Api Endpoint Services
+
+            // 2. Use Application Use Case Services
+
+            // 3. UseData - Infrastructure Services
+            app.UseMigration<BasketDbContext>();
             return app;
         }
     }
